778ec8a15b793625f0fb906ef2e71d2447ef55b2
André Gonçalves <44908082+Imm3rs1on@users.noreply.github.com>
2022-11-10 14:18:22 +0000

    Add Andre's Files

---
+++ src/main/java/com/appdetex/harvest/marketplace/AbstractHarvesterJsoup.java
@@ -0,0 +1,37 @@
+package com.appdetex.harvest.marketplace;
+
+import com.appdetex.harvest.api.HarvestException;
+import com.appdetex.harvest.api.MarketplaceDetection;
+import com.appdetex.harvest.api.MarketplaceHarvester;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.List;
+
+public abstract class AbstractHarvesterJsoup implements MarketplaceHarvester {
+    private String baseurl;
+    private String userAgent = "appdetex";
+
+    public AbstractHarvesterJsoup(String baseurl) {
+        this.baseurl = baseurl;
+    }
+
+    @Override
+    public List<MarketplaceDetection> parseTarget(String searchTerm, int numItems) throws HarvestException {
+        int pageOrder = 0;
+
+        try {
+            Document doc = Jsoup.connect(baseurl).userAgent(userAgent).get();
+            System.out.printf("Title: %s\n\n\n\n", doc.title());
+            return parseTarget(doc, numItems);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new HarvestException();
+        }
+    }
+
+
+    protected abstract List<MarketplaceDetection> parseTarget(Document doc, int numItems) throws HarvestException;
+}
---
+++ src/main/java/com/appdetex/harvest/marketplace/AmazonUsHarvester.java
@@ -0,0 +1,8 @@
+package com.appdetex.harvest.marketplace;
+
+public class AmazonUsHarvester extends AbstractAmazonHarvester {
+
+    public AmazonUsHarvester() {
+        super("https://www.amazon.com/s?k=\" + searchTerm + \"&crid=FQLYPINM8486&sprefix=\" + searchTerm + \"%2Caps%2C297&ref=nb_sb_noss_1");
+    }
+}
