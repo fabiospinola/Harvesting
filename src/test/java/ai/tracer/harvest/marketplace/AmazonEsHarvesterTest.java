6ede5f9af89904f58e1220bb998b90e65a1ac124
Fábio Spínola <fabio.spinola@appdetex.com>
2022-12-21 17:36:35 +0000

    commented individual harvester tests

--- src/test/java/ai/tracer/harvest/marketplace/AmazonEsHarvesterTest.java
+++ src/test/java/ai/tracer/harvest/marketplace/AmazonEsHarvesterTest.java
@@ -2,9 +2,10 @@ package ai.tracer.harvest.marketplace;
 
 import ai.tracer.harvest.marketplace.amazonplaywright.AmazonESHarvester;
 
-public class AmazonEsHarvesterTest extends AbstractAmazonEUHarvesterTest {
+/*public class AmazonEsHarvesterTest extends AbstractAmazonEUHarvesterTest {
 
     public AmazonEsHarvesterTest(){
         super(new AmazonESHarvester(), "file:///Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsSearch.html","file:///Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsItem.html");
     }
 }
+*/
