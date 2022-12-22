3c4c625d709ca2866c7107a5487fb2263c7e786d
Fábio Spínola <fabio.spinola@appdetex.com>
2022-12-22 10:09:28 +0000

    uncommented amazonplaywright tests for a jenkins try

--- src/test/java/ai/tracer/harvest/marketplace/AbstractAmazonEUHarvesterTest.java
+++ src/test/java/ai/tracer/harvest/marketplace/AbstractAmazonEUHarvesterTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
-/*abstract class AbstractAmazonEUHarvesterTest {
+abstract class AbstractAmazonEUHarvesterTest {
     private String pageWithResults;
     private AbstractAmazonEUHarvester harvester;
     private String singleItem;
@@ -30,4 +30,4 @@ import java.util.List;
             throw new RuntimeException(e);
         }
     }
-}*/
+}
--- src/test/java/ai/tracer/harvest/marketplace/AmazonEsHarvesterTest.java
+++ src/test/java/ai/tracer/harvest/marketplace/AmazonEsHarvesterTest.java
@@ -2,10 +2,9 @@ package ai.tracer.harvest.marketplace;
 
 import ai.tracer.harvest.marketplace.amazonplaywright.AmazonESHarvester;
 
-/*public class AmazonEsHarvesterTest extends AbstractAmazonEUHarvesterTest {
+public class AmazonEsHarvesterTest extends AbstractAmazonEUHarvesterTest {
 
     public AmazonEsHarvesterTest(){
         super(new AmazonESHarvester(), "file:///Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsSearch.html","file:///Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsItem.html");
     }
 }
-*/
