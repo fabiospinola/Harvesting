9f996501dce6a96fc0e9d9fd8d523b1ed211710c
Fábio Spínola <fabio.spinola@appdetex.com>
2023-01-16 09:35:06 +0000

    changes in tests

--- src/test/java/ai/tracer/harvest/harvesters/AmazonEsHarvesterTest.java
+++ src/test/java/ai/tracer/harvest/harvesters/AmazonEsHarvesterTest.java
@@ -5,6 +5,6 @@ import ai.tracer.harvest.harvesters.amazonplaywright.AmazonEsHarvester;
 public class AmazonEsHarvesterTest extends AbstractAmazonPlaywrightTest {
 
     public AmazonEsHarvesterTest(){
-        super(new AmazonEsHarvester(), "file:///Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsSearch.html","file:///Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsItem.html");
+        super(new AmazonEsHarvester(), "file:/Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsSearch.html","file:///Users/fabio.spinola/Documents/WebScraping/Harvesting/WebScraper/src/test/resources/amazonEsItem.html");
     }
 }
